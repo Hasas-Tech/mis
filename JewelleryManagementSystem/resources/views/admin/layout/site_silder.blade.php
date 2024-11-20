fd820bac2e219c2e3bb7024af0a345ef347d4337
Salahuddin Safi <zaydsafi9@gmail.com>
2024-11-20 16:43:36 +0430

    Dashboard page route created successfully

---
+++ JewelleryManagementSystem/resources/views/admin/layout/site_silder.blade.php
@@ -0,0 +1,143 @@
+<div id="dw-s1" class="bmd-layout-drawer bg-faded">
+
+    <div class="container-fluid side-bar-container">
+        <header class="pb-0">
+            <a class="navbar-brand">
+                <object class="side-logo" data="{{ asset('assets/svg/logo-8.svg') }}" type="image/svg+xml">
+                </object>
+            </a>
+        </header>
+        <p class="side-comment">Tour</p>
+        <!-- <li class="side a-collapse short ">
+            <a href="./fa.html" class="side-item "><i class="fas fa-language mr-1"></i>Persian <span
+                    class="badge badge-pill badge-success">new</span></a>
+        </li> -->
+        <li class="side a-collapse short ">
+            <a href="./" class="side-item selected"><i class="fas fa-home mr-1"></i>Dashboard</a>
+        </li>
+
+        <ul class="side a-collapse short">
+            <a class="ul-text"><i class="fab fa-usps mr-1"></i> Posts
+                <!-- <span class="badge badge-info">4</span> -->
+                <i class="fas fa-chevron-down arrow"></i></a>
+            <div class="side-item-container ">
+                <li class="side-item "><a href="./"><i class="fas fa-angle-right mr-2"></i>Dashboard</a>
+                </li>
+                <li class="side-item"><a href="./dark.html"><i class="fas fa-angle-right mr-2"></i>Dark
+                        Dashboard</a></li>
+                <li class="side-item"><a href="./Login.html"><i class="fas fa-angle-right mr-2"></i>Login</a>
+                </li>
+                <li class="side-item"><a href="./glogin.html"><i class="fas fa-angle-right mr-2"></i>Login
+                        Colored</a></li>
+            </div>
+        </ul>
+
+        <ul class="side a-collapse short">
+            <a class="ul-text"><i class="fas fa-scroll mr-1"></i> Pages
+                <!-- <span class="badge badge-info">4</span> -->
+                <i class="fas fa-chevron-down arrow"></i></a>
+            <div class="side-item-container ">
+                <li class="side-item"><a href="./"><i class="fas fa-angle-right mr-2"></i>Dashboard</a>
+                </li>
+                <li class="side-item"><a href="./dark.html"><i class="fas fa-angle-right mr-2"></i>Dark
+                        Dashboard</a></li>
+                <li class="side-item"><a href="./Login.html"><i class="fas fa-angle-right mr-2"></i>Login</a>
+                </li>
+                <li class="side-item"><a href="./glogin.html"><i class="fas fa-angle-right mr-2"></i>Login
+                        Colored</a></li>
+            </div>
+        </ul>
+
+        <ul class="side a-collapse short">
+            <a class="ul-text"><i class="fab fa-slideshare mr-1"></i> Slider
+                <!-- <span class="badge badge-info">4</span> -->
+                <i class="fas fa-chevron-down arrow"></i></a>
+            <div class="side-item-container">
+                <li class="side-item"><a href="./"><i class="fas fa-angle-right mr-2"></i>Dashboard</a>
+                </li>
+                <li class="side-item"><a href="./dark.html"><i class="fas fa-angle-right mr-2"></i>Dark
+                        Dashboard</a></li>
+                <li class="side-item"><a href="./Login.html"><i class="fas fa-angle-right mr-2"></i>Login</a>
+                </li>
+                <li class="side-item"><a href="./glogin.html"><i class="fas fa-angle-right mr-2"></i>Login
+                        Colored</a></li>
+            </div>
+        </ul>
+
+        <ul class="side a-collapse short">
+            <a class="ul-text"><i class="fas fa-th-list mr-1"></i> Categories
+                <!-- <span	class="badge badge-success">4</span> -->
+                <i class="fas fas fa-chevron-down arrow"></i></a>
+            <div class="side-item-container hide animated">
+                <li class="side-item"><a href="./color.html"><i class="fas fa-angle-right mr-2"></i>Color</a>
+                </li>
+                <li class="side-item"><a href="./typo.html"><i
+                            class="fas fa-angle-right mr-2"></i>Typography</a></li>
+                <li class="side-item"><a href="./dark-mode.html"><i class="fas fa-angle-right mr-2"></i>Dark
+                        Mode</a></li>
+                <li class="side-item"><a href="./rtl.html"><i class="fas fa-angle-right mr-2"></i>Rtl</a></li>
+                <li class="side-item"><a href="./sidebar.html"><i
+                            class="fas fa-angle-right mr-2"></i>SideBar</a></li>
+            </div>
+        </ul>
+
+        <ul class="side a-collapse short">
+            <a class="ul-text"><i class="fas fa-bars mr-1"></i> Menu
+                <!-- <span	class="badge badge-success">4</span> -->
+                <i class="fas fas fa-chevron-down arrow"></i></a>
+            <div class="side-item-container hide animated">
+                <li class="side-item"><a href="./color.html"><i class="fas fa-angle-right mr-2"></i>Color</a>
+                </li>
+                <li class="side-item"><a href="./typo.html"><i
+                            class="fas fa-angle-right mr-2"></i>Typography</a></li>
+                <li class="side-item"><a href="./dark-mode.html"><i class="fas fa-angle-right mr-2"></i>Dark
+                        Mode</a></li>
+                <li class="side-item"><a href="./rtl.html"><i class="fas fa-angle-right mr-2"></i>Rtl</a></li>
+                <li class="side-item"><a href="./sidebar.html"><i
+                            class="fas fa-angle-right mr-2"></i>SideBar</a></li>
+            </div>
+        </ul>
+
+        <ul class="side a-collapse short">
+            <a class="ul-text"><i class="fas fa-poll mr-1"></i> Polls
+                <!-- <span	class="badge badge-success">4</span> -->
+                <i class="fas fas fa-chevron-down arrow"></i></a>
+            <div class="side-item-container hide animated">
+                <li class="side-item"><a href="./color.html"><i class="fas fa-angle-right mr-2"></i>Color</a>
+                </li>
+                <li class="side-item"><a href="./typo.html"><i
+                            class="fas fa-angle-right mr-2"></i>Typography</a></li>
+                <li class="side-item"><a href="./dark-mode.html"><i class="fas fa-angle-right mr-2"></i>Dark
+                        Mode</a></li>
+                <li class="side-item"><a href="./rtl.html"><i class="fas fa-angle-right mr-2"></i>Rtl</a></li>
+                <li class="side-item"><a href="./sidebar.html"><i
+                            class="fas fa-angle-right mr-2"></i>SideBar</a></li>
+            </div>
+        </ul>
+
+        <ul class="side a-collapse short">
+            <a class="ul-text"><i class="fas fa-cog fa-spin mr-1"></i> Setting
+                <!-- <span	class="badge badge-success">4</span> -->
+                <i class="fas fas fa-chevron-down arrow"></i></a>
+            <div class="side-item-container hide animated">
+                <li class="side-item"><a href="./updateSetting.html"><i class="fas fa-angle-right mr-2"></i>Update Setting</a>
+                </li>
+            </div>
+        </ul>
+
+        <ul class="side a-collapse short">
+            <a class="ul-text"><i class="fas fa-users-cog mr-1"></i> User Management
+                <!-- <span	class="badge badge-success">4</span> -->
+                <i class="fas fas fa-chevron-down arrow"></i></a>
+            <div class="side-item-container hide animated">
+                <li class="side-item"><a href="./newUser.html"><i class="fas fa-angle-right mr-2"></i>New User</a>
+                </li>
+                <li class="side-item"><a href="./allUsers.html"><i
+                            class="fas fa-angle-right mr-2"></i>All Users</a></li>
+                <li class="side-item"><a href="./dark-mode.html"><i class="fas fa-angle-right mr-2"></i>New Role</a></li>
+                <li class="side-item"><a href="./rtl.html"><i class="fas fa-angle-right mr-2"></i>All Roles</a></li>
+            </div>
+        </ul>
+    </div>
+
+</div>
