3451729aae785b4b8a213be474c0cf45592a71f9
Salahuddin Safi <zaydsafi9@gmail.com>
2024-12-05 15:03:00 +0430

    foreign key

--- jms/database/migrations/2014_10_12_000000_create_users_table.php
+++ jms/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role_type');
-            $table->string('role_type');
+            $table->foreign('role_type')->references('role_type')->on('roles');
+            $table->string('image');
             $table->rememberToken();
             $table->timestamps();
         });
