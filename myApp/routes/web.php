a8d4b5eca979c84e37e1adc27774773d8f3dd373
Salahuddin Safi <zaydsafi9@gmail.com>
2025-01-16 23:31:52 +0430

    route updated

--- myApp/routes/web.php
+++ myApp/routes/web.php
@@ -4,8 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//
+Route::get('/', [DashboardController::class, 'dashboard'])->name('dashboard');
 Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
 Route::get('/login', [LoginController::class, 'login'])->name('login');
